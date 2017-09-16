79124504af1706bacc4021a9f22dc7c547553016
biswajit077 <creckbiswajit@gmail.com>
2017-09-17 00:05:36 +0600

    Update

---
+++ All/MVC/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+
+namespace MVC.Models
+{
+    [Table("Persons", Schema = "dbo")]
+    public class Person
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactNumber { get; set; }
+    }
+}
