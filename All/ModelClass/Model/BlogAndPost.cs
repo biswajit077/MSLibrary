bc4e9fba6a29d3a48798421f937cdacee8d641b8
biswajit077 <creckbiswajit@gmail.com>
2017-09-28 22:26:41 +0600

    Added New Project (Model)

---
+++ All/ModelClass/Model/BlogAndPost.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace ModelClass.Model
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string BlogName { get; set; }
+        public virtual List<Post>Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string PostTitle{ get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public virtual Blog Blog { get; set; }
+    }
+}
